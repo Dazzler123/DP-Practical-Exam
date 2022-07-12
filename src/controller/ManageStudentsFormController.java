0f0db4480d9742d655d08e23d5a4acebc04cf1ec
Dazzler123 <dasinduhewagamage@gmail.com>
2022-07-12 10:24:00 +0530

    Initialized manage students UI

--- src/controller/ManageStudentsFormController.java
+++ src/controller/ManageStudentsFormController.java
@@ -1,4 +1,34 @@
 package controller;
 
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
 public class ManageStudentsFormController {
+    public TableView tblStudent;
+    public TableColumn colID;
+    public TableColumn colName;
+    public TableColumn colEmail;
+    public TableColumn colContact;
+    public TableColumn colAddress;
+    public TableColumn colNIC;
+    public JFXTextField txtID;
+    public JFXTextField txtName;
+    public JFXTextField txtEmail;
+    public JFXTextField txtContact;
+    public JFXTextField txtAddress;
+    public JFXTextField txtNIC;
+
+    public void txtSearchStudent(ActionEvent actionEvent) {
+    }
+
+    public void btnAddNewStudent(ActionEvent actionEvent) {
+    }
+
+    public void btnUpdateStudent(ActionEvent actionEvent) {
+    }
+
+    public void btnDeleteStudent(ActionEvent actionEvent) {
+    }
 }
